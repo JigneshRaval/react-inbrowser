8e3ec382648b9a88bc491904728522ac78c1f523
jignesh <jignesh.raval@azilen.com>
2016-01-13 12:57:39 +0530

    Minor changes

--- server.js
+++ server.js
@@ -1,54 +1,36 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+
 http.createServer(function (request, response) {
-    console.log('request starting...', request.url);
-	
-	var filePath = '.' + request.url;
-	if (filePath == './')
-		filePath = './index.html';
-		
-	var extname = path.extname(filePath);
-	var contentType = 'text/html';
-	switch (extname) {
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.css':
-			contentType = 'text/css';
-			break;
-	}
-  
-    fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(content, 'utf-8');
-				}
-			});
-	/*
-	path.exists(filePath, function(exists) {
-	
-		if (exists) {
-			fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(content, 'utf-8');
-				}
-			});
-		}
-		else {
-			response.writeHead(404);
-			response.end();
-		}
-	});*/
-	
+  console.log('request starting...', request.url);
+
+  var filePath = '.' + request.url;
+  if (filePath == './')
+    filePath = './index.html';
+
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+  }
+
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      response.writeHead(500);
+      response.end();
+    } else {
+      response.writeHead(200, {
+        'Content-Type': contentType
+      });
+      response.end(content, 'utf-8');
+    }
+  });
 }).listen(3000);
+
 console.log('Server running at http://localhost:3000/');
